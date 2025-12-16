a832d09caf60eed7d3ebed87b87039c3382198ec
humasfebunila <117450024+humasfebunila@users.noreply.github.com>
2025-12-16 07:55:12 +0700

    Create header.php

---
+++ header.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="id">
+<head>
+    <meta charset="UTF-8">
+    <title>PPID BPS</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+<header>
+    <h1>PPID BPS</h1>
+    <nav>
+        <a href="index.php">Informasi Publik</a>
+        <a href="permohonan_form.php">Ajukan Permohonan / Keberatan</a>
+    </nav>
+</header>
+<main>
